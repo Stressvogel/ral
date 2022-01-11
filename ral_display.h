b58f7a77e5fe498178c667399413309729856643
hypothermic <32428346+hypothermic@users.noreply.github.com>
2022-01-11 16:07:26 +0100

    Refactor code

--- ral_display.h
+++ ral_display.h
@@ -2,22 +2,24 @@
 // Created by Aran on 02/12/2021.
 //
 
-#ifndef SOFTWARE_RAL_H
-#define SOFTWARE_RAL_H
+#ifndef SOFTWARE_RAL_DISPLAY_H
+#define SOFTWARE_RAL_DISPLAY_H
 
 #include <cstdint>
 
-class RAL {
+namespace ral {
+
+class display {
 protected:
     uint16_t width;
     uint8_t height;
-public:
 
+public:
     /**
      * Init functie.
      * Wordt aangeroepen zodra de game wordt geinitialiseerd.
      **/
-    virtual void ral_init() = 0;
+    virtual void init() = 0;
 
     /**
      * Set de kleur van de pixel op X,Y
@@ -26,7 +28,7 @@ public:
      * @param y       Y-coordinaat
      * @param color   5-6-5 RGB code in een 16-bit integer
      **/
-    virtual void ral_draw_pixel(uint16_t x, uint8_t y, uint16_t color) = 0;
+    virtual void draw_pixel(uint16_t x, uint8_t y, uint16_t color) = 0;
 
     /**
      * Teken een vierkant
@@ -37,7 +39,7 @@ public:
      * @param height  Hoogte
      * @param color   5-6-5 RGB code in een 16-bit integer
      **/
-    virtual void ral_draw_box(uint16_t x, uint8_t y, uint16_t width, uint8_t height, uint16_t color, bool fill = true) = 0;
+    virtual void draw_box(uint16_t x, uint8_t y, uint16_t width, uint8_t height, uint16_t color, bool fill = true) = 0;
 
     /**
      * Teken een sprite
@@ -48,12 +50,12 @@ public:
      * @param height  Hoogte
      * @param sprite  Pointer naar 2d array met de pixel data van de sprite
      **/
-    virtual void ral_draw_sprite(uint16_t x, uint8_t y, uint16_t width, uint8_t height, uint16_t ***sprite) = 0;
+    virtual void draw_sprite(uint16_t x, uint8_t y, uint16_t width, uint8_t height, uint16_t ***sprite) = 0;
 
     /**
  	 * Clear screen
  	 **/
-    virtual void ral_clear() = 0;
+    virtual void clear() = 0;
 
     virtual uint16_t get_width() = 0;
 
@@ -61,5 +63,6 @@ public:
 
 };
 
+}
 
-#endif //SOFTWARE_RAL_H
+#endif //SOFTWARE_RAL_DISPLAY_H
---
+++ ral_display_vga.h
@@ -0,0 +1,69 @@
+//
+// Created by Aran on 02/12/2021.
+//
+
+#ifndef SOFTWARE_VGA_H
+#define SOFTWARE_VGA_H
+
+#include <altera_up_avalon_video_dma_controller.h>
+#include <cstdio>
+#include <system.h>
+#include <stdint.h>
+
+#include "ral_display.h"
+
+namespace ral {
+
+/**
+ * RAL-implementatie voor het aansturen van een beeldscherm via VGA.<br />
+ * <br />
+ * Het maakt gebruik van de UP Video drivers.
+ **/
+class display_vga : public display {
+protected:
+    alt_up_video_dma_dev *video_dev;
+
+public:
+    display_vga();
+
+    virtual ~display_vga();
+
+    /**
+     * @inheritDoc
+     **/
+    void init();
+
+    /**
+     * @inheritDoc
+     **/
+    void draw_pixel(uint16_t x, uint8_t y, uint16_t color);
+
+    /**
+     * @inheritDoc
+     **/
+    void draw_box(uint16_t x, uint8_t y, uint16_t width, uint8_t height, uint16_t color, bool fill = true);
+
+    /**
+     * @inheritDoc
+     **/
+    void draw_sprite(uint16_t x, uint8_t y, uint16_t width, uint8_t height, uint16_t ***sprite);
+
+    /**
+     * @inheritDoc
+     **/
+    void clear();
+
+    /**
+     * @inheritDoc
+     **/
+    uint16_t get_width();
+
+    /**
+     * @inheritDoc
+     **/
+    uint8_t get_height();
+};
+
+} // namespace ral
+
+#endif //SOFTWARE_VGA_H
